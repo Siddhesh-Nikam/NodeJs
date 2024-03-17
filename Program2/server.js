edc20651627c5cf26a7c10bb6092edf65b9a7cf7
Siddhesh Nikam <98577535+Siddhesh-Nikam@users.noreply.github.com>
2024-03-17 19:15:16 +0530

    Add files via upload

---
+++ Program2/server.js
@@ -0,0 +1,46 @@
+// console.log("Hello Duniya");
+
+// function Add(No1,No2)
+// {
+//     return No1+No2;
+// }
+
+// Result = Add(21,11);
+// console.log(Result);
+
+//2
+
+// var Add = function (No1,No2)
+// {
+//     return No1+No2;
+// }
+
+// Result = Add(21,11);
+// console.log(Result);
+
+//3
+
+// var Add = (No1,No2) => {return No1+No2}
+
+// Result = Add(21,11);
+// console.log(Result);
+
+//4
+
+// var Add = (No1,No2) => No1+No2;
+
+// Result = Add(21,11);
+// console.log(Result);
+
+//5
+
+(function Add(No1,No2)
+{
+    let result = No1+No2;
+    console.log(result)
+})(10,11)
+
+
+
+
+
